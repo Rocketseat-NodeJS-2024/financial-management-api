e53f8701bf40400a698ccda4d820fddd4f510aa1
Victor Rodrigues <victor.carvalho@isalab.com.br>
2024-03-30 15:50:21 -0300

    feat: :sparkles: initial commit

---
+++ src/database.ts
@@ -0,0 +1,16 @@
+import { knex as setupKnex, Knex } from "knex";
+import { env } from "./env";
+
+const config: Knex.Config = {
+  client: "sqlite",
+  connection: { filename: env.DATABASE_URL },
+  useNullAsDefault: true,
+  migrations: {
+    extension: "ts",
+    directory: "./db/migrations",
+  },
+};
+
+const knex = setupKnex(config);
+
+export { knex, config };
